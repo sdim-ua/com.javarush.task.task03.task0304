de2e49c4913516f1b5108b78624e1517d7a59612
sdim-ua <ingersol@yandex.ua>
2019-01-04 10:57:20 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,16 @@
+/* 
+Задача на проценты
+
+*/
+
+public class Solution {
+    public static double addTenPercent(int i) {
+        //напишите тут ваш код
+        return i * 1.1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addTenPercent(9));
+    }
+}
+
